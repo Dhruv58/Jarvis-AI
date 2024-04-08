c38cfe4f13b0a5c7c67e8c1f5fabeb1514a4b3e6
dhruv deshmukh <dhruvdeshmukh820@gmail.com>
2024-04-08 20:26:36 +0530

    added image assets

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -34,7 +34,7 @@ from multiprocessing.pool import ThreadPool
 from deco_rator import *
 # from main import wish_me,take_user_input
 
-from online import find_my_ip, play_on_youtube,search_on_google,search_on_wikipedia,send_email,get_latest_news,get_random_joke,get_weather_report
+from online import find_my_ip, youtube,search_on_google,search_on_wikipedia,send_email,get_news,weather_forecast
 
 # Set the width and height of the screen
 width, height = 1920, 1080
@@ -69,6 +69,23 @@ def speak(text):
   
 @threaded      
 def take_command():
+    # try:    
+    #     r = sr.Recognizer()
+    #     with sr.Microphone() as source:
+    #         print("Listening...")
+    #         r.pause_threshold = 1
+    #         audio = r.listen(source)
+
+    #     print("Recognizing....")
+    #     query = r.recognize_google(audio, language='en-in')
+    #     print(query)
+    #     return query.lower()
+
+    # except Exception as e:
+    #     print(e)
+    #     speak("Sorry I couldn't understand. Can you please repeat that?")
+    #     return 'None'
+    
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
@@ -93,6 +110,8 @@ def take_command():
         speak("Sorry I couldn't understand. Can you please repeat that?")
         queri = 'None'
     return queri
+    
+    
 
 
 
@@ -126,7 +145,7 @@ class CircleWidget(Widget):
         self.volume_history = [0,0,0,0,0,0,0]
 
         # By increasing the size of volume_history_size, you can make the transition of size more smooth.
-        self.volume_history_size = 40
+        self.volume_history_size = 140
 
         # Define relative minimal and maximal sizes for your circle
         self.min_size = .2 * SCREEN_WIDTH
@@ -148,7 +167,7 @@ class CircleWidget(Widget):
         # Schedule the update function for the time label
         Clock.schedule_interval(self.update_time, 1)
         
-        self.title = Label(text='[b][color=3333ff]ERROR BY NIGHT[/color][/b]', font_size=42, markup=True, font_name='mw.ttf', pos=(920, 900))
+        self.title = Label(text='[b][color=3333ff]ERROR BY NIGHT[/color][/b]', font_size=42, markup=True, font_name='dusri.ttf', pos=(920, 900))
         self.add_widget(self.title)
 
         self.subtitles_input = TextInput(
@@ -189,12 +208,13 @@ class CircleWidget(Widget):
     def run_speech_recognition(self):
         r = sr.Recognizer()
         with sr.Microphone() as source:
-            # r.pause_threshold=1
+            r.pause_threshold=1
             audio = r.listen(source)
 
         query = r.recognize_google(audio,language="en-in")
-        
+        # query = take_command()
         # Update the GUI from the main thread using Clock.schedule_once
+    
         Clock.schedule_once(lambda dt: setattr(self.subtitles_input, 'text', query))
         CircleWidget.handle_jarvis_commands(query.lower())
         return query.lower()
@@ -262,58 +282,58 @@ class CircleWidget(Widget):
         self.stream.start()
        
     def handle_jarvis_commands(query):
-    
-        if "how are you" in query:
-            speak("I am fine how are you.")
+        try:
+            if "how are you" in query:
+                speak("I am fine how are you.")
         
-        elif "open command prompt" in query:
-            speak("Opening command prompt")
-            os.system('start cmd')
+            elif "open command prompt" in query:
+                speak("Opening command prompt")
+                os.system('start cmd')
 
-        elif "open camera" in query:
-            speak("Opening camera sir")
-            sp.run('start microsoft.windows.camera:', shell=True)
+            elif "open camera" in query:
+                speak("Opening camera sir")
+                sp.run('start microsoft.windows.camera:', shell=True)
 
-        elif "open notepad" in query:
-            speak("Opening Notepad for you sir")
-            notepad_path = "C:\\Users\\ASUS\\AppData\\Local\\Microsoft\\WindowsApps\\notepad.exe"
-            os.startfile(notepad_path)
+            elif "open notepad" in query:
+                speak("Opening Notepad for you sir")
+                notepad_path = "C:\\Users\\ASUS\\AppData\\Local\\Microsoft\\WindowsApps\\notepad.exe"
+                os.startfile(notepad_path)
 
-        elif "open discord" in query:
-            speak("Opening Discord for you sir")
-            discord_path = "C:\\Users\\ASUS\\AppData\\Local\\Discord\\app-1.0.9028\\Discord.exe"
-            os.startfile(discord_path)
+            elif "open discord" in query:
+                speak("Opening Discord for you sir")
+                discord_path = "C:\\Users\\ASUS\\AppData\\Local\\Discord\\app-1.0.9028\\Discord.exe"
+                os.startfile(discord_path)
 
-        elif "open gta" in query:
-            speak("Opening Gta for you sir")
-            gta_path = "D:\\Tanishq\\GTA\\Launcher.exe"
-            os.startfile(gta_path)
+            elif "open gta" in query:
+                speak("Opening Gta for you sir")
+                gta_path = "D:\\Tanishq\\GTA\\Launcher.exe"
+                os.startfile(gta_path)
         
 
-        elif 'subscribe' in query:
-            speak(
-                        "Everyone who are watching this video, Please subscribe for more amazing content from error by "
-                        "night. I will show you how to do this")
-            speak("Firstly Go to youtube")
-            webbrowser.open("https://www.youtube.com/")
-            speak("click on the search bar")
-            pyautogui.moveTo(806, 125, 1)
-            pyautogui.click(x=806, y=125, clicks=1, interval=0, button='left')
-            speak("Error by night")
-            pyautogui.typewrite("Error by night", 0.1)
-            time.sleep(1)
-            speak("press enter")
-            pyautogui.press('enter')
-            pyautogui.moveTo(971, 314, 1)
-            speak("Here you will see our channel")
-            pyautogui.moveTo(1688, 314, 1)
-            speak("click here to subscribe our channel")
-            pyautogui.click(x=1688, y=314, clicks=1, interval=0, button='left')
-            speak("And also Don't forget to press the bell icon")
-            pyautogui.moveTo(1750, 314, 1)
-            pyautogui.click(x=1750, y=314, clicks=1, interval=0, button='left')
-            speak("turn on all notifications")
-            pyautogui.click(x=1750, y=320, clicks=1, interval=0, button='left')
+            elif 'subscribe' in query:
+                speak(
+                            "Everyone who are watching this video, Please subscribe for more amazing content from error by "
+                            "night. I will show you how to do this")
+                speak("Firstly Go to youtube")
+                webbrowser.open("https://www.youtube.com/")
+                speak("click on the search bar")
+                pyautogui.moveTo(806, 125, 1)
+                pyautogui.click(x=806, y=125, clicks=1, interval=0, button='left')
+                speak("Error by night")
+                pyautogui.typewrite("Error by night", 0.1)
+                time.sleep(1)
+                speak("press enter")
+                pyautogui.press('enter')
+                pyautogui.moveTo(971, 314, 1)
+                speak("Here you will see our channel")
+                pyautogui.moveTo(1688, 314, 1)
+                speak("click here to subscribe our channel")
+                pyautogui.click(x=1688, y=314, clicks=1, interval=0, button='left')
+                speak("And also Don't forget to press the bell icon")
+                pyautogui.moveTo(1750, 314, 1)
+                pyautogui.click(x=1750, y=314, clicks=1, interval=0, button='left')
+                speak("turn on all notifications")
+                pyautogui.click(x=1750, y=320, clicks=1, interval=0, button='left')
 
         # elif 'calculate' in query:
         #             app_id = ""
@@ -354,36 +374,36 @@ class CircleWidget(Widget):
         #                 f'Your IP Address is {ip_address}.\n For your convenience, I am printing it on the screen sir.')
         #             print(f'Your IP Address is {ip_address}')
 
-        elif 'search on wikipedia' in query:
-                    speak('What do you want to search on Wikipedia, sir?')        
-                    return_val = take_command().result_queue.get()
-                    results = search_on_wikipedia(return_val)
-                    speak(f"According to Wikipedia, {results}")
-                    # speak("For your convenience, I am printing it on the screen sir.")
-                    # print(results)
+            elif 'search on wikipedia' in query:
+                        speak('What do you want to search on Wikipedia, sir?')        
+                        return_val = take_command().result_queue.get()
+                        results = search_on_wikipedia(return_val)
+                        speak(f"According to Wikipedia, {results}")
+                        # speak("For your convenience, I am printing it on the screen sir.")
+                        # print(results)
 
-        elif 'youtube' in query:
-                    speak('What do you want to play on Youtube, sir?')
-                    video = take_command().result_queue.get()
-                    play_on_youtube(video)
+            elif 'youtube' in query:
+                        speak('What do you want to play on Youtube, sir?')
+                        video = take_command().result_queue.get()
+                        youtube(video)
 
-        elif 'search on google' in query:
-                    speak('What do you want to search on Google, sir?')
-                    query = take_command().result_queue.get()
-                    search_on_google(query)
+            elif 'search on google' in query:
+                        speak('What do you want to search on Google, sir?')
+                        query = take_command().result_queue.get()
+                        search_on_google(query)
 
-        elif "send an email" in query:
-                    speak("On what email address do I send sir? Please enter in the console: ")
-                    receiver_address = input("Enter email address: ")
-                    speak("What should be the subject sir?")
-                    subject = take_command().result_queue.get()
-                    speak("What is the message sir?")
-                    message = take_command().result_queue.get()
-                    if send_email(receiver_address, subject, message):
-                        speak("I've sent the email sir.")
-                        print("I've sent the email sir.")
-                    else:
-                        speak("Something went wrong while I was sending the mail. Please check the error logs sir.")
+            elif "send an email" in query:
+                        speak("On what email address do I send sir? Please enter in the console: ")
+                        receiver_address = input("Enter email address: ")
+                        speak("What should be the subject sir?")
+                        subject = take_command().result_queue.get()
+                        speak("What is the message sir?")
+                        message = take_command().result_queue.get()
+                        if send_email(receiver_address, subject, message):
+                            speak("I've sent the email sir.")
+                            print("I've sent the email sir.")
+                        else:
+                            speak("Something went wrong while I was sending the mail. Please check the error logs sir.")
 
         # elif 'tell me any joke' in query:
         #             speak(f"Hope you like this one sir")
@@ -392,47 +412,48 @@ class CircleWidget(Widget):
         #             speak("For your convenience, I am printing it on the screen sir.")
         #             pprint(joke)
 
-        elif 'movie' in query:
-                    movies_db = imdb.IMDb()
-                    speak("please tell me the movie name :")
-                    text = take_command().result_queue.get()
-                    movies = movies_db.search_movie(text)
-                    speak("Searching for" + text)
-                    speak("I Found these: ")
-                    for movie in movies:
-                        title = movie["title"]
-                        year = movie["year"]
-                        speak(f"{title}-{year}")
-                        info = movie.getID()
-                        movie_info = movies_db.get_movie(info)
-                        rating = movie_info["rating"]
-                        cast = movie_info["cast"]
-                        actor = cast[0:5]
-                        plot = movie_info.get('plot outline', 'Plot summary not available')
-                        speak(
-                            f'{title} was released in {year} has imdb ratings of {rating}.It has a cast of {actor} . The plot summary'
-                            f'of movie is {plot}')
-                        print(
-                            f'{title} was released in {year} has imdb ratings of {rating}.It has a cast of {actor} The plot summary '
-                            f'of movie is {plot}')
+            elif 'movie' in query:
+                        movies_db = imdb.IMDb()
+                        speak("please tell me the movie name :")
+                        text = take_command().result_queue.get()
+                        movies = movies_db.search_movie(text)
+                        speak("Searching for" + text)
+                        speak("I Found these: ")
+                        for movie in movies:
+                            title = movie["title"]
+                            year = movie["year"]
+                            speak(f"{title}-{year}")
+                            info = movie.getID()
+                            movie_info = movies_db.get_movie(info)
+                            rating = movie_info["rating"]
+                            cast = movie_info["cast"]
+                            actor = cast[0:5]
+                            plot = movie_info.get('plot outline', 'Plot summary not available')
+                            speak(
+                                f'{title} was released in {year} has imdb ratings of {rating}.It has a cast of {actor} . The plot summary'
+                                f'of movie is {plot}')
+                            print(
+                                f'{title} was released in {year} has imdb ratings of {rating}.It has a cast of {actor} The plot summary '
+                                f'of movie is {plot}')
 
-        elif 'give me news' in query:
-                    
-                    speak(f"I'm reading out the latest news hea`dlines, sir")
-                    speak(get_latest_news())
-                    
+            elif 'give me news' in query:
+                        
+                        speak(f"I'm reading out the latest news hea`dlines, sir")
+                        speak(get_news())
+                        
 
-        elif 'weather' in query:
-                    ip_address = find_my_ip()
-                    city = 'indore'
-                    speak(f"Getting weather report for your city {city}")
-                    weather, temperature, feels_like = get_weather_report(city)
-                    speak(f"The current temperature is {temperature}, but it feels like {feels_like}")
-                    speak(f"Also, the weather report talks about {weather}")
-                    speak("For your convenience, I am printing it on the screen sir.")
-                    print(f"Description: {weather}\nTemperature: {temperature}\nFeels like: {feels_like}")
+            elif 'weather' in query:
+                        ip_address = find_my_ip()
+                        city = 'indore'
+                        speak(f"Getting weather report for your city {city}")
+                        weather, temperature, feels_like = weather_forecast(city)
+                        speak(f"The current temperature is {temperature}, but it feels like {feels_like}")
+                        speak(f"Also, the weather report talks about {weather}")
+                        speak("For your convenience, I am printing it on the screen sir.")
+                        print(f"Description: {weather}\nTemperature: {temperature}\nFeels like: {feels_like}")
            
-        
+        except Exception as e:
+            print(e)
         
 
         
